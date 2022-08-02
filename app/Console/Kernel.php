4824cf74d71549aa24b0e98278b9c96cf94589be
zheka <poliv78@gmail.com>
2022-08-02 08:10:27 +0200

    test

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -7,7 +7,7 @@ use Laravel\Lumen\Console\Kernel as ConsoleKernel;
 
 class Kernel extends ConsoleKernel
 {
-    /**
+    /** test12345
      * The Artisan commands provided by your application.
      *
      * @var array
